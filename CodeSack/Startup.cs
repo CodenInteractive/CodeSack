049aaca807c9a813318e09b74d033ba69cbfa154
Denis <Denis@ADVP-DCORMIER>
2016-04-27 13:47:06 -0300

    Add project files.

---
+++ CodeSack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeSack.Startup))]
+namespace CodeSack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
